5d8308b935abe803c712d2848e82886e69090bdf
Bak Nguen <sangsangden@gmail.com>
2026-05-22 07:43:51 +0700

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:manato_web/core/route/router.dart';
+import 'package:manato_web/firebase_options.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sizer/sizer.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'core/theme/app_theme.dart';
 
-void main() {
+void main() async{
    setPathUrlStrategy();
-   
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
